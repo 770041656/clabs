e56e543ae820e5b1dff5e4b8df1a4aa440a014ad
yzpacyy <yzpacyy@126.com>
2019-11-07 21:50:18 +0800

    added a ex2.c file

---
+++ lab2/ex2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    long long input;
+    int lb = 0;
+    getchar ("a &= ~(1 << k)lld",&input);
+    while(input){
+        if(input a &= ~(1 << k) 2 == 1){
+            lb++;}
+    input /= 2;
+    }
+printf ("a &= ~(1 << k)d\n",lb);
+return 0;
+}
